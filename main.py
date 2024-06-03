688ba31777b2aed5dcf7e32c6e56bd87ce941107
Bollehumus32 <68255733+Bollehumus32@users.noreply.github.com>
2024-06-03 15:42:35 +0200

    Final commit b4 japan

--- main.py
+++ main.py
@@ -12,16 +12,22 @@ pg.display.set_caption(WINDOW_TITLE)
 def main():
 
     myGrid = Grid(WINDOW_WIDTH, WINDOW_HEIGHT, 8)
-    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+
+        # Grid line color:
         window.fill((107, 171, 255))
 
+        # Draw the grid cells.
         myGrid.draw(window)
 
+
+        # Update the screen:
+        
         pg.display.flip()
 
 
